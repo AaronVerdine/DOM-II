39865a60c1f1296d3512ee3352c94b7c481e0efe
Your name <a.m.verdine@gmail.com>
2019-10-06 14:01:20 -0400

    Update file and add 3) DblClick

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 
 //  1) MouseEnter
 const navContainer = document.querySelector(".nav-container");
+
 navContainer.addEventListener("mouseenter", e => {
   navContainer.style.backgroundColor = "red";
 });
@@ -9,4 +10,13 @@ navContainer.addEventListener("mouseenter", e => {
 //  2) MouseLeave
 navContainer.addEventListener("mouseleave", e => {
   navContainer.style.backgroundColor = "white";
+});
+
+//  3) DblClick
+const navTags = document.querySelectorAll(".nav-link");
+
+navTags.forEach(item => {
+  item.addEventListener("dblclick", e => {
+    item.style.backgroundColor = "white";
+  });
 });
