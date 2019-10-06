337e7780388867f8761a54f079862d63b497b1e8
Your name <a.m.verdine@gmail.com>
2019-10-06 10:23:30 -0400

    Update file with 1) mouseenter & 2) mouseleave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+//  1) MouseEnter
+const navContainer = document.querySelector(".nav-container");
+navContainer.addEventListener("mouseenter", e => {
+  navContainer.style.backgroundColor = "red";
+});
+
+//  2) MouseLeave
+navContainer.addEventListener("mouseleave", e => {
+  navContainer.style.backgroundColor = "white";
+});
