a36c1af3683de97d3957a41b20b9803e825e5daa
Your name <a.m.verdine@gmail.com>
2019-10-07 01:07:41 -0400

    Update file and add 4, 5, 6, 7

--- js/index.js
+++ js/index.js
@@ -20,3 +20,46 @@ navTags.forEach(item => {
     item.style.backgroundColor = "white";
   });
 });
+
+// 4) MouseOver
+const logoHead = document.querySelector(".logo-heading");
+
+logoHead.addEventListener("mouseover", e => {
+  logoHead.style.backgroundColor = "white";
+  logoHead.style.color = "green";
+});
+
+// 5) MouseOut
+logoHead.addEventListener("mouseout", e => {
+  logoHead.style.backgroundColor = "red";
+  logoHead.style.color = "black";
+  // e.stopPropagation();
+});
+//  Try using StopProoagation on this event listener
+
+// 6) ContextMenu
+const busImg = document.querySelector(".intro img");
+
+busImg.addEventListener("contextmenu", e => {
+  e.preventDefault();
+});
+
+//  7) Wheel
+const mapImg = document.querySelector(".content-section img");
+// const mapImg = document.querySelectorAll(".section img");
+
+mapImg.addEventListener("wheel", e => {
+  event.preventDefault();
+
+  scale += event.deltaY * -0.01;
+
+  // Restrict scale
+  scale = Math.min(Math.max(0.125, scale), 4);
+
+  // Apply scale transform
+  mapImg.style.transform = `scale(${scale})`;
+});
+
+let scale = 1;
+
+const funBtn = document.addEventListener(".destination .btn");
